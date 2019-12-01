6f216c9be1bd898f2c98cfc0cf29699b47e93c68
RonaldoCMS <ronaldocms@outlook.it>
2019-12-01 21:33:59 +0100

    Es01

--- ES01/Catalogo.java
+++ ES01/Catalogo.java
@@ -1,54 +1,140 @@
-package ListinoCatalogo;
-
+/***
+ * La classe Catalogo gestisce le operazioni da effettuare alla classe Telefono
+ * @author FABIO
+ * @version v1.1
+ */
 public class Catalogo {
+
+	//ATTRIBUTI
 	
-	Telefono telefono = new Telefono();
-	private int codice;
-	private double prezzo;
-	private int qty;
-	private int temp;
-	
-	public Catalogo() {
-		codice=0;
-		prezzo=0;
-		qty=0;
-		temp=0;
+	 private Telefono[] telefoni;
+	 private int telefoniVenduti;
+	 private int numeroTelefoni;
+	 
+	 
+	 //COSTRUTTORE
+	 public Catalogo() {
+		 telefoni = new Telefono[100];
+		 telefoniVenduti = 0;
+		 numeroTelefoni = 0;
+	 }
+
+
+	 /***
+	  * Aggiungi un telefono nel catalogo
+	  * 
+	  */
+	 public void addTelefono(Telefono telefono) 
+	    {
+	        this.telefoni[numeroTelefoni] = new Telefono(telefono);  
+	        this.numeroTelefoni+=1;
+	    }
+	 
+	 /***
+	  * Rimuovi telefono nel catalogo
+	  */
+	 public void removeTelefono(int codice) {
+		 Telefono telefono;
+		 
+		 telefono = getTelefono(codice);
+		 
+		 if( telefono != null) {
+			 
+			 int i = getIndiceTelefono(telefono);
+			 for (int j = i +1; j < this.numeroTelefoni; j++) {
+	                    telefoni[j-1] = telefoni[j];
+	            }
+	            numeroTelefoni--;
+		 }
+	 }
+	 
+	  /***
+	   * Espelli un telefono
+	   */
+	    public int vendiTelefono(int codice) {
+	    	
+	        int res = -2;
+	        
+	        for (int i = 0; i < numeroTelefoni; i++) {
+	        	
+	            if (codice == telefoni[i].getCodice()) {
+	                res = telefoni[i].subQty(1);
+	            }
+	        }
+	        return res;
+	    }
+	    
+	   /***
+	    * Visualizzaa la quantità di tutti i cellulari 
+	    * @return quantità totale
+	    */
+	    public int getAllQty() 
+	    {
+	        int disp = 0;
+	        for (int i = 0; i < this.numeroTelefoni; i++) 
+	        {
+	            disp = disp + this.telefoni[i].getQty();
+	        }
+	        return disp;
+	    }
+	    
+ 
+    /***
+     * Visualizza telefono desiderato tramite codice
+     * @param codice
+     * @return Telefono
+     */
+	 public Telefono getTelefono(int codice)  {
+	        Telefono telefono = null;
+	        for (int i = 0; i < this.numeroTelefoni; i++) {
+	            if (codice == this.telefoni[i].getCodice()) {
+	                telefono = telefoni[i];
+	                i = 101;
+	            }
+	        }
+	        return telefono;
+	    }
+
+	public void listino() {
+		for(int i=0; i<numeroTelefoni; i++) {
+			System.out.println(telefoni[i].toString());
+			}
 	}
 	
-	public double getPrezzo() { return prezzo; }
-	public void setPrezzo(double prezzo) { this.prezzo = prezzo; }
-	public int getQty() { return qty; }
-	public void setQty(int qty) { this.qty = qty; }
-	public int getCodice() { return codice; }	
-	public void setCodice(int codice) { this.codice = codice; }
-	
-	public String toString() {
-		return "\tPrezzo: " + prezzo
-			 + "\tQuantità:" + qty
-			 + "\tCODICE:" + codice;
+	//METODI GETTER E SETTER
+	public Telefono[] getTelefoni() {
+		return telefoni;
 	}
 	
-	public int addQty(int temp) {
-		if(temp>0)
-			qty+=temp;
-		
-		else {
-			System.out.println("Errore! valore inserito è uguale o minore di 0");
-		}
-		return qty;
-	}
-	
-	public int subQty(int temp) {
-		if(temp>0)
-			qty-=temp;
-		
-		else {
-			System.out.println("Errore! valore inserito è uguale o minore di 0");
-		}
-		
-		return qty;
+	public void setTelefoni(Telefono[] telefoni) {
+		this.telefoni = telefoni;
 	}
 
+	public int getTelefoniVenduti() {
+		return telefoniVenduti;
+	}
+
+	public void setTelefoniVenduti(int telefoniVenduti) {
+		this.telefoniVenduti = telefoniVenduti;
+	}
+
+	public int getNumeroTelefoni() {
+		return numeroTelefoni;
+	}
+
+	public void setNumeroTelefoni(int numeroTelefoni) {
+		this.numeroTelefoni = numeroTelefoni;
+	}
 	
-	
+	//METODO INTERNO ALLA CLASSE
+	private int getIndiceTelefono(Telefono telefono) {
+		 
+		int i;
+		for (i = 0; i < this.numeroTelefoni; i++)  {
+			if (telefono.getCodice() == this.telefoni[i].getCodice()) {
+			return i;
+			}
+		}
+		return 0;	
+	}
 }
--- ES01/Telefono.java
+++ ES01/Telefono.java
@@ -1,60 +1,191 @@
-package ListinoCatalogo;
+/**
+ * La classe Telefono gestisce i dati di un telefono
+ * e permette di modificare la quantità dei telefoni
+ * @author FABIO
+ * @version v1.1
+ *
+ */
+public class Telefono {
 
-	public class Telefono {
-		
-		private String marca;
-		private String cpu;
-		private int ram;
-		private int memory;
-		private int autonomia;
-		
-		public Telefono() {
-			setMarca("Valore costruttore");
-			setCpu("Valore costruttore");
-			setRam(0);
-			setMemory(0);
-			setAutonomia(0);
-	}
-
-		public String getMarca() {
-			return marca;
-		}
-		public void setMarca(String marca) {
-			this.marca = marca;
-		}
-		public String getCpu() {
-			return cpu;
-		}
-		public void setCpu(String cpu) {
-			this.cpu = cpu;
-		}
-		public int getRam() {
-			return ram;
-		}
-		public void setRam(int ram) {
-			this.ram = ram;
-		}
-		public int getMemory() {
-			return memory;
-		}
-		public void setMemory(int memory) {
-			this.memory = memory;
-		}
-		public int getAutonomia() {
-			return autonomia;
-		}
-		public void setAutonomia(int autonomia) {
-			this.autonomia = autonomia;
-		}
-		public String toString() {
-
-			return "Marca: " + marca 
-				+ "\tCPU: " + cpu
-				+ "\tRam: " + ram
-				+ "\tMemoria interna: " + memory
-				+ "\tAutonomia: " + autonomia;
-			}
-		
-		
-}
+	//ATTRIBUTI
 	
+	protected String marca;
+	protected String modello;
+	protected int codice;
+	protected int ram;
+	protected int memoria;
+	protected int qty;
+	protected double prezzo;
+	
+	//COSTRUTTORI
+	
+	public Telefono() {
+		marca = "";
+		modello = "";
+		codice=0;
+		ram=0;
+		memoria=0;
+		qty=0;
+		prezzo=0;
+	}
+	
+	public Telefono(Telefono tel) {
+		marca = tel.getMarca();
+		modello = tel.getModello();
+		codice= tel.getCodice();
+		ram= tel.getRam();
+		memoria= tel.getMemoria();
+		qty= tel.getQty();
+		prezzo= tel.getPrezzo();
+	}
+	
+	public Telefono(String marca, String modello,
+					int codice, int ram, int memoria, int qty,
+					double prezzo) {
+		
+		this.marca = marca;
+		this.modello = modello;
+		this.codice = codice;
+		this.ram = ram;
+		this.memoria = memoria;
+		this.qty = qty;
+		this.prezzo = prezzo;
+	}
+	
+	
+	//METODI
+	
+	/**
+	 * Ritorna la marca del telefono
+	 */
+	public String getMarca() {
+		return marca;
+	}
+	
+	/**
+	 * Inserisci marca del telefono
+	 */
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	
+	/*
+	 * Ritorna il modello del telefono
+	 */
+	public String getModello() {
+		return modello;
+	}
+	
+	/**
+	 * inserisci il modello del telefono
+	 */
+	public void setModello(String modello) {
+		this.modello = modello;
+	}
+	
+	/**
+	 * Ritorna il codice del telefono 
+	 */
+	public int getCodice() {
+		return codice;
+	}
+	
+	/**
+	 * Inserisci il codice del telefono 
+	 */
+	public void setCodice(int codice) {
+		this.codice = codice;
+	}
+	
+	/**
+	 * Ritorna ram del telefono
+	 */
+	public int getRam() {
+		return ram;
+	}
+	
+	/**
+	 * Inserisci ram del telefono
+	 */
+	public void setRam(int ram) {
+		this.ram = ram;
+	}
+	
+	/**
+	 * Ritorna memoria del telefono
+	 */
+	public int getMemoria() {
+		return memoria;
+	}
+	
+	/**
+	 * Inserisci memoria del telefono
+	 */
+	public void setMemoria(int memoria) {
+		this.memoria = memoria;
+	}
+	
+	/**
+	 * Ritorna quantita di telefoni
+	 */
+	public int getQty() {
+		return qty;
+	}
+	
+	/**
+	 * Inserisce quantita' di telefoni
+	 */
+	public int setQty(int qty) {
+		return this.qty = qty;
+	}
+	
+	/**
+	 * Aggiungi nuovi telefoni
+	 * @param x Quantità da aggiungere
+	 * @return Quantità modificata
+	 */
+	public int addQty(int x) {
+		return qty+=x;
+	}
+	
+	/**
+	 * Rimuovi telefoni
+	 * @param x numero di telefoni da eliminare
+	 * @return numero telefoni aggiornato
+	 */
+	public int subQty(int x) {
+		return qty-=x;
+	}
+	
+	/**
+	 * Ritorna prezzo
+	 */
+	public double getPrezzo() {
+		return prezzo;
+	}
+	
+	/**
+	 * Inserisci prezzo
+	 */
+	public void setPrezzo(double prezzo) {
+		this.prezzo = prezzo;
+	}
+	
+	/***
+	 * Visualizza tutte le info del telefono (Utilizzabile con System.out.print)
+	 */
+	@Override
+	public String toString() {
+		return "marca=" + marca +
+				", modello=" + modello +
+				", codice=" + codice +
+				", ram=" + ram +
+				", memoria=" + memoria + 
+				", qty=" + qty + 
+				", prezzo=" + prezzo;
+				
+	}
+	
+	
+	
+}
